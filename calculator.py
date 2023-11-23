bafd18a2d84422f45522148acc6d72f769daa521
EEpraf <fevedvfv2201@naver.com>
2023-11-23 13:29:44 +0900

    premium branch

--- calculator.py
+++ calculator.py
@@ -1,23 +1,14 @@
 #Calculator
 def add(a, b):
-    return a + b;
+    return a + b
 
 def sub(a, b):
-    return a - b;
+    return a - b
 
+#Multiply
 def mul(a, b):
-    return a * b;
+    return a * b
 
-def div(a, b):
-    if (b == 0):
-        print("No");
-    else:
-        return a / b;
-
-def main():
-    print(add(1, 2));
-    print(div(1, 0));
-    
-    return 0;
-
-main();
+#Divide
+def div_main(a, b):
+    return a / b
